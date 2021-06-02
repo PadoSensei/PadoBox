da318a7eede5dbf2578d9fd77f196899ecfbcf76
PadoSensei <padraigo.donoghue@gmail.com>
2021-06-02 17:27:55 +0100

    Updates

---
+++ precache-manifest.cc5f27b695b36ac709ee32bbf8185873.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a6f25d460be1e8a0bf60d77b5c158a",
+    "url": "/PadoBox/index.html"
+  },
+  {
+    "revision": "2fe35a00504ce45ea944",
+    "url": "/PadoBox/static/css/2.eb1025ee.chunk.css"
+  },
+  {
+    "revision": "bbe6b9dfd91dae705458",
+    "url": "/PadoBox/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "2fe35a00504ce45ea944",
+    "url": "/PadoBox/static/js/2.b9fcc2d4.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/PadoBox/static/js/2.b9fcc2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe6b9dfd91dae705458",
+    "url": "/PadoBox/static/js/main.12a01c77.chunk.js"
+  },
+  {
+    "revision": "4a56875ec648476a6085",
+    "url": "/PadoBox/static/js/runtime-main.b0fe8f7a.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/PadoBox/static/media/box.df8b84d5.png"
+  }
+]);
